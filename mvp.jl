89bb358cb53fbf7e5d5fd6e780a4632590894cd2
Matteo Bertorotta <m.f.bertorotta@student.tudelft.nl>
2025-03-05 01:09:36 +0100

    refactored intersection algo and added todos

--- mvp.jl
+++ mvp.jl
@@ -25,10 +25,10 @@ using Metatheory.EGraphs
 # println(g₂)
 
 t₁ = @theory a begin
-  true --> :a ≥ zero
+  true --> :a ≥ :zero
   a --> cond(true, a, -a)
   -(-a) == a
-  -zero == zero
+  -(:zero) == :zero
 end
 
 g₁ = EGraph{Expr, Nothing}(:(a))
@@ -36,15 +36,33 @@ saturate!(g₁, t₁)
 println(g₁)
 
 t₂ = @theory a begin
-  false --> :a ≥ zero
+  false --> :a ≥ :zero
   -a --> cond(false, a, -a)
   -(-a) == a
-  -zero == zero
+  -(:zero) == :zero
 end
 
 g₂ = EGraph{Expr, Nothing}(:(-a))
 saturate!(g₂, t₂)
 println(g₂)
+
+"""
+For each node in an eclass, get its root constant
+Then return a dictionary with the constant as key and the node as value
+"""
+function get_eclass_constants(g::EGraph, c::EClass)
+  constants = Dict{Symbol, VecExpr}()
+  for node in c.nodes  
+      v = v_head(node)
+      c = get_constant(g, v)
+      if typeof(c) != Symbol
+        println(typeof(c))
+        c = Symbol(c) #TODO find out why this is necessary
+      end
+      constants[c] = node
+    end
+  return constants
+end
 
 """
 Interstects two eclasses
@@ -54,30 +72,17 @@ Go on until all equal expressions are found
 """
 function eclass_intersect(g₁::EGraph, g₂::EGraph, c₁::EClass=g₁[g₁.root], c₂::EClass=g₂[g₂.root], seen=Set{Tuple{Id, Id}}())
 
+  #TODO add memoization
+  #I think you can record that something isnt seen yet, is being explored, or has been explored
   !in((c₁.id, c₂.id), seen) || (return false)
   push!(seen, (c₁.id, c₂.id))
 
-  c₁_constants = Dict{Symbol, VecExpr}()
-  for node in c₁.nodes
-      v = v_head(node)
-      c = get_constant(g₁, v)
-      if typeof(c) != Symbol
-        c = Symbol(c)
-      end
-      c₁_constants[c] = node
-  end
-
-  c₂_constants = Dict{Symbol, VecExpr}()
-  for node in c₂.nodes
-      v = v_head(node)
-      c = get_constant(g₂, v)
-      if typeof(c) != Symbol
-        c = Symbol(c)
-      end
-      c₂_constants[c] = node
-  end
+  c₁_constants = get_eclass_constants(g₁, c₁)
+  c₂_constants = get_eclass_constants(g₂, c₂)
 
   common_constants = intersect(Set{Symbol}(collect(keys(c₁_constants))), Set{Symbol}(collect(keys(c₂_constants))))
+
+  #TODO define set of extracted programs
 
   for c in common_constants
     node₁ = c₁_constants[c]
@@ -86,26 +91,32 @@ function eclass_intersect(g₁::EGraph, g₂::EGraph, c₁::EClass=g₁[g₁.roo
     children₁ = v_children(node₁)
     children₂ = v_children(node₂)
 
-    all_equal = true
+    # if the nodes are constants, they must be equal
+    length(children₁) > 0 || return true #TODO add constant to extracted programs and continue
 
-    if length(children₁) > 0
-      for i in eachindex(children₁)
-        seen_child = copy(seen)
-        child₁ = children₁[i]
-        child₂ = children₂[i]
-        c_child₁ = g₁[child₁]
-        c_child₂ = g₂[child₂]
-        all_equal = all_equal && eclass_intersect(g₁, g₂, c_child₁, c_child₂, seen_child)
+    # else, for each child, recursively check if they have representations of that child that are equal
+    all_equal = true
+    #TODO make an array. for each i, store the found programs
+    for i in eachindex(children₁)
+      seen_child = copy(seen) # every recursion branch should have its own seen set
+      child₁ = children₁[i]
+      child₂ = children₂[i]
+      c_child₁ = g₁[child₁]
+      c_child₂ = g₂[child₂]
+      if !eclass_intersect(g₁, g₂, c_child₁, c_child₂, seen_child) #TODO change to != nothing
+        all_equal = false
+        break
       end
     end
+    #TODO for each combination of found programs, perform a maketerm() and add to extracted programs
 
     if all_equal
-      return true #TODO change to returning all common expressions
+      return true #TODO return all extracted programs
     end    
     
   end
 
-  return false
+  return false #TODO return Nothing
 end
 
 eclass_intersect(g₁, g₂)
