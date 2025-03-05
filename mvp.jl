7242ec947656a64c6e708a2ada7e896d321fedff
Matteo Bertorotta <m.f.bertorotta@student.tudelft.nl>
2025-03-05 23:49:28 +0100

    intersection algorithm finds all programs
    only the maketerm does not yet work

--- mvp.jl
+++ mvp.jl
@@ -1,6 +1,7 @@
 using Metatheory
 using Metatheory.Library
 using Metatheory.EGraphs
+using IterTools
 
 #compare this with using true and false
 
@@ -74,7 +75,7 @@ function eclass_intersect(g₁::EGraph, g₂::EGraph, c₁::EClass=g₁[g₁.roo
 
   #TODO add memoization
   #I think you can record that something isnt seen yet, is being explored, or has been explored
-  !in((c₁.id, c₂.id), seen) || (return false)
+  !in((c₁.id, c₂.id), seen) || (return nothing)
   push!(seen, (c₁.id, c₂.id))
 
   c₁_constants = get_eclass_constants(g₁, c₁)
@@ -82,7 +83,8 @@ function eclass_intersect(g₁::EGraph, g₂::EGraph, c₁::EClass=g₁[g₁.roo
 
   common_constants = intersect(Set{Symbol}(collect(keys(c₁_constants))), Set{Symbol}(collect(keys(c₂_constants))))
 
-  #TODO define set of extracted programs
+  # define set of extracted programs
+  found_programs = Vector()
 
   for c in common_constants
     node₁ = c₁_constants[c]
@@ -91,32 +93,42 @@ function eclass_intersect(g₁::EGraph, g₂::EGraph, c₁::EClass=g₁[g₁.roo
     children₁ = v_children(node₁)
     children₂ = v_children(node₂)
 
-    # if the nodes are constants, they must be equal
-    length(children₁) > 0 || return true #TODO add constant to extracted programs and continue
+    # if the nodes have no children, they must be equal and complete
+    if length(children₁) == 0 
+      push!(found_programs, c) # add constant to extracted programs and continue
+      continue 
+    end
 
     # else, for each child, recursively check if they have representations of that child that are equal
-    all_equal = true
-    #TODO make an array. for each i, store the found programs
+    all_child_programs = Vector() # make an array. for each i, store the found programs
     for i in eachindex(children₁)
       seen_child = copy(seen) # every recursion branch should have its own seen set
       child₁ = children₁[i]
       child₂ = children₂[i]
       c_child₁ = g₁[child₁]
       c_child₂ = g₂[child₂]
-      if !eclass_intersect(g₁, g₂, c_child₁, c_child₂, seen_child) #TODO change to != nothing
-        all_equal = false
-        break
-      end
+      child_programs = eclass_intersect(g₁, g₂, c_child₁, c_child₂, seen_child)
+      child_programs !== nothing || break
+      push!(all_child_programs, child_programs)
     end
-    #TODO for each combination of found programs, perform a maketerm() and add to extracted programs
 
-    if all_equal
-      return true #TODO return all extracted programs
+    if length(all_child_programs) == length(children₁)
+      #return true 
+      combinations = collect(IterTools.product(all_child_programs...))
+      for combination in combinations
+        # for each combination of all_child_programs, perform a maketerm() and add to found_programs
+        p = maketerm(Expr, c, combination, nothing)
+        push!(found_programs, p)
+      end
     end    
     
   end
 
-  return false #TODO return Nothing
+  if isempty(found_programs)
+    return nothing
+  else
+    return found_programs
+  end
 end
 
 eclass_intersect(g₁, g₂)
